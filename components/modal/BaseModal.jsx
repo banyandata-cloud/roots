7350d39d49495c703303e169eb33044225c2a9ab
Jaidev Singh Bhui <b.jaidev@banyancloud.io>
2022-10-18 18:35:29 +0530

    refac(Modal): removed react-modal dep; using popper

--- components/modal/BaseModal.jsx
+++ components/modal/BaseModal.jsx
@@ -1,38 +1,42 @@
-import Modal from 'react-modal';
+import { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { classes } from '../../utils';
 import Cross from '../icons/Cross/Cross';
 import styles from './BaseModal.module.css';
-
-Modal.setAppElement('#root');
+import { Popper } from '../popper';
+import { useOutsideClickListener } from '../../hooks';
 
 const BaseModal = (props) => {
 	const { className, renderHeader, children, renderFooter, toggle, open } = props;
 
+	const ref = useRef(null);
+
+	useOutsideClickListener(ref, () => {
+		return toggle(false);
+	});
+
 	if (open) {
 		return (
-			<Modal
-				isOpen={open}
-				className={classes(className, styles.content)}
-				overlayClassName={styles.overlay}
-				onRequestClose={toggle}>
-				{renderHeader && (
-					<div data-elem='header' className={styles.header}>
-						{renderHeader}
+			<Popper open={open} transparent={false} id='base-modal-popper'>
+				<div className={classes(styles.root, className)} ref={ref}>
+					{renderHeader && (
+						<div data-elem='header' className={styles.header}>
+							{renderHeader}
+						</div>
+					)}
+					<div data-elem='body' className={styles.body}>
+						{children}
 					</div>
-				)}
-				<div data-elem='body' className={styles.body}>
-					{children}
-				</div>
-				{renderFooter && (
-					<div data-elem='footer' className={styles.footer}>
-						{renderFooter}
+					{renderFooter && (
+						<div data-elem='footer' className={styles.footer}>
+							{renderFooter}
+						</div>
+					)}
+					<div className={styles.closeModal} onClick={toggle}>
+						<Cross className={styles.closeIcon} />
 					</div>
-				)}
-				<div className={styles.closeModal} onClick={toggle}>
-					<Cross className={styles.closeIcon} />
 				</div>
-			</Modal>
+			</Popper>
 		);
 	}
 	return null;
