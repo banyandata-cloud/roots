3e407868cbe4afa21ce3aec56e47b209c0c03446
Jaidev Singh Bhui <b.jaidev@banyancloud.io>
2022-10-25 13:36:41 +0530

    added build script w/ rollup as bundler

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+
+const packageJson = require('./package.json');
+
+export default [
+	{
+		input: 'index.js',
+		output: [
+			{
+				file: packageJson.main,
+				format: 'cjs',
+				sourcemap: true,
+			},
+			{
+				file: packageJson.module,
+				format: 'esm',
+				sourcemap: true,
+			},
+		],
+		plugins: [
+			babel({
+				extensions: ['.jsx', '.js', '.tsx'],
+				exclude: 'node_modules/**',
+			}),
+			resolve({
+				extensions: ['.jsx', '.js', '.tsx'],
+				exclude: 'node_modules/**',
+			}),
+			postcss(),
+			commonjs(),
+		],
+	},
+	{
+		input: 'dist/esm/index.js',
+		output: [
+			{
+				file: 'dist/index.js',
+				format: 'esm',
+			},
+		],
+	},
+];
