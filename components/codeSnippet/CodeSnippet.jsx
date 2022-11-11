45ee80244bfca1389ea087c253d3afd45316a0d4
pradeep <pradeep@banyancloud.io>
2022-11-11 10:09:25 +0530

    refactor(CodeSnippet): imported Copy as CopyIcon and added default value of fill in path of copyIcon

--- components/codeSnippet/CodeSnippet.jsx
+++ components/codeSnippet/CodeSnippet.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { dracula, atomOneLight, CodeBlock } from 'react-code-blocks';
 import { classes } from '../../utils';
-import Copy from '../icons/Copy/Copy';
+import { CopyIcon } from '../icons';
 import styles from './CodeSnippet.module.css';
 
 const CodeSnippet = (props) => {
@@ -33,7 +33,7 @@ const CodeSnippet = (props) => {
 			/>
 			{copy && (
 				<div className={styles.copy} onClick={onCopy}>
-					<Copy
+					<CopyIcon
 						className={classes(
 							styles.icon,
 							theme === 'dark' ? styles.dark : styles.light
