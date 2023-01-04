81094ec21fe87f7f96b11b00eb3c42341d093443
Aryan-BanyanCloud <d.aryan@banyancloud.io>
2023-01-04 10:47:43 +0530

    Added Region Map Chart and New Desgin in Pie Chart

--- components/charts/pieChart/BasePieChart.stories.jsx
+++ components/charts/pieChart/BasePieChart.stories.jsx
@@ -73,6 +73,7 @@ Default.args = {
 	},
 	cursor: 'default',
 	radius: ['30%', '60%'],
+	center: ['50%', '50%'],
 	seriesData: sampleData,
 	semiDoughnut: false,
 	startAngle: 90,
@@ -110,6 +111,12 @@ Default.args = {
 		// 	return '214 Safe';
 		// },
 		// icon: 'path://M30.9,53.2C16.8,53.2,5.3,41.7,5.3,27.6S16.8,2,30.9,2C45,2,56.4,13.5,56.4,27.6S45,53.2,30.9,53.2z M30.9,3.5C17.6,3.5,6.8,14.4,6.8,27.6c0,13.3,10.8,24.1,24.101,24.1C44.2,51.7,55,40.9,55,27.6C54.9,14.4,44.1,3.5,30.9,3.5z M36.9,35.8c0,0.601-0.4,1-0.9,1h-1.3c-0.5,0-0.9-0.399-0.9-1V19.5c0-0.6,0.4-1,0.9-1H36c0.5,0,0.9,0.4,0.9,1V35.8z M27.8,35.8 c0,0.601-0.4,1-0.9,1h-1.3c-0.5,0-0.9-0.399-0.9-1V19.5c0-0.6,0.4-1,0.9-1H27c0.5,0,0.9,0.4,0.9,1L27.8,35.8L27.8,35.8z',
+		// icon: '<svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+		// <rect y="0.5" width="16" height="16" rx="4" fill="#FF626A"/>
+		// eslint-disable-next-line max-len
+		// <path d="M11.25 6.5L7 10.75L4.75 8.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+		// </svg>
+		// ',
 	},
 	seriesOption: [
 		{
