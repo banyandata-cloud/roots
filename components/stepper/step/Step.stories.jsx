135f80f633f510fbb578ee865fd8391b9bedd3a9
pradeep <pradeep@banyancloud.io>
2023-03-10 17:04:59 +0530

    refac(Step): tail-separator (conditional)

---
+++ components/stepper/step/Step.stories.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Step from './Step';
+
+export default {
+	title: 'Components/Stepper/Step',
+	component: Step,
+	parameters: {
+		options: {
+			options: {
+				showToolbar: true,
+			},
+		},
+		docs: {
+			description: {
+				component: 'The steps',
+			},
+		},
+	},
+};
+
+const Template = (args) => {
+	return (
+		<div>
+			<Step {...args} />
+		</div>
+	);
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+	title: null,
+	description: null,
+	active: false,
+	completion: 1,
+	error: false,
+	index: 0,
+};
+
+Default.parameters = {
+	design: {
+		type: 'figma',
+		url: 'https://www.figma.com/file/e9opoAtQHBo4vFd6u0Co98/Component-Library?node-id=266%3A49880',
+	},
+};
