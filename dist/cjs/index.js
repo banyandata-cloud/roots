3f112743da9977039803aa56a902a34863d6eb79
PDP-Banyan <PDP-Banyan@users.noreply.github.com>
2023-05-19 05:35:09 +0000

    npm publish - open PR - added tag

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -113574,15 +113574,16 @@ var BaseMap = function BaseMap(props) {
       }));
     }
   }, [clustered, children, map]);
+  var childCount = React.Children.count(children);
   React.useEffect(function () {
-    if (fitBounds && map && React.Children.count(children) > 0) {
+    if (fitBounds && map && childCount > 0) {
       var bounds = new google.maps.LatLngBounds();
       React.Children.forEach(children, function (child) {
         bounds.extend(new google.maps.LatLng(child.props.position.lat, child.props.position.lng));
       });
       map.fitBounds(bounds);
     }
-  }, [fitBounds, map]);
+  }, [fitBounds, map, childCount]);
   useDeepCompareEffectForMaps(function () {
     if (map) {
       map.setOptions(options);
