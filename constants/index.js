85b36334f62f56fa51e6967d1bcb976d23ff5f53
pradeep <pradeep@banyancloud.io>
2023-06-05 16:53:49 +0530

    refac(constants): removed base url from roots: keeping the config within app

--- constants/index.js
+++ constants/index.js
@@ -1,30 +1,3 @@
-// BASE URLS
-const auth = process.env.REACT_APP_AUTH_BASE_URL;
-const encs = process.env.REACT_APP_ENCS_BASE_URL;
-const scos = process.env.REACT_APP_SCOS_BASE_URL;
-const aws = process.env.REACT_APP_AWS_BASE_URL;
-const gcp = process.env.REACT_APP_GCP_BASE_URL;
-const oci = process.env.REACT_APP_OCI_BASE_URL;
-const pgsql = process.env.REACT_APP_PGSQL_BASE_URL;
-const mysql = process.env.REACT_APP_MYSQL_BASE_URL;
-const oracle = process.env.REACT_APP_ORACLE_BASE_URL;
-const lmosGcp = process.env.REACT_APP_AUDIT_LOG_GCP_BASE_URL;
-const lmosAws = process.env.REACT_APP_AUDIT_LOG_AWS_BASE_URL;
-
-export const BASE_URLS = {
-	auth,
-	encs,
-	scos,
-	aws,
-	gcp,
-	oci,
-	'lmos-aws': lmosAws,
-	'lmos-gcp': lmosGcp,
-	pgsql,
-	mysql,
-	oracle,
-};
-
 export const MONTHS = [
 	'Jan',
 	'Feb',
