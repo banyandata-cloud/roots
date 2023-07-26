c7274f9e939d90dc28be3ee4f4202459563c6494
“BhanuPrakashBC” <g.bhanuprakash@banyancloud.io>
2023-07-26 17:27:23 +0530

    Created new Slider component

---
+++ components/input/slider1/Slider.stories.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Slider from './Slider';
+
+export default {
+	title: 'Components/Slider',
+	component: Slider,
+	argTypes: {
+		onChange: {
+			action: 'Volume Changed',
+		},
+	},
+};
+
+const Template = (args) => {
+	return (
+		<div
+			style={{
+				padding: '20px',
+				maxWidth: '400px',
+			}}>
+			<Slider {...args} />
+		</div>
+	);
+};
+
+export const Default = Template.bind({});
+Default.args = {
+	label: 'Volume Control',
+	min: 0,
+	max: 100,
+};
+
+export const DisableSlider = Template.bind({});
+DisableSlider.args = {
+	label: 'Volume Control',
+	defaultChecked: true,
+	min: 0,
+	max: 10,
+	step: 5,
+	disabled: true,
+};
+
+export const UncontrolledSlider = Template.bind({});
+UncontrolledSlider.args = {
+	label: 'Volume Control',
+	value: 0,
+	min: 0,
+	max: 10,
+	step: 5,
+};
+
+export const ControlledSlider = Template.bind({});
+ControlledSlider.args = {
+	label: 'Volume Control',
+	minLabel: 'Low',
+	medLabel: 'Medium',
+	maxLabel: 'High',
+	defaultChecked: true,
+	min: 0,
+	max: 10,
+	step: 5,
+};
