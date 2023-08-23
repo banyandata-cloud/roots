c3aa8b520619494e1ce6d5a0d78b8d24ad769421
Aryan-BanyanCloud <d.aryan@banyancloud.io>
2023-08-23 12:16:44 +0530

    Documentation : Widget

---
+++ components/widget/BaseWidgetStoriesData.js
@@ -0,0 +1,54 @@
+export const CHART_DATA = {
+	1: {
+		'Networking Services': {
+			x1: 66.66,
+			x2: 33.33,
+		},
+		'IAM&Admin': {
+			x1: 66.66,
+			x2: 33.33,
+		},
+		'Cloud SQL': {
+			x1: 66.66,
+			x2: 33.33,
+		},
+		'VPC Network': {
+			x1: 66.66,
+			x2: 33.33,
+		},
+		'Compute Engine': {
+			x1: 66.66,
+			x2: 33.33,
+		},
+		'Cloud Storage': {
+			x1: 66.66,
+			x2: 33.33,
+		},
+	},
+	2: {
+		'Networking Services': {
+			x1: 33.33,
+			x2: 66.66,
+		},
+		'IAM&Admin': {
+			x1: 33.33,
+			x2: 66.66,
+		},
+		'Cloud SQL': {
+			x1: 33.33,
+			x2: 66.66,
+		},
+		'VPC Network': {
+			x1: 33.33,
+			x2: 66.66,
+		},
+		'Compute Engine': {
+			x1: 33.33,
+			x2: 66.66,
+		},
+		'Cloud Storage': {
+			x1: 33.33,
+			x2: 66.66,
+		},
+	},
+};
