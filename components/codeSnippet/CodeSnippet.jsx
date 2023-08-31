4b9b0eda8839fb8bc5590a8d4c4050863f527cf6
manas-banyancloud <c.manas@banyancloud.io>
2023-08-31 10:51:39 +0530

    :art: Error boundary - CodeSnippet

--- components/codeSnippet/CodeSnippet.jsx
+++ components/codeSnippet/CodeSnippet.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/forbid-prop-types */
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark, oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { classes } from '../../utils';
 import { CopyIcon } from '../icons';
 import styles from './CodeSnippet.module.css';
+import { ErrorBoundaryWrapper } from '../errorBoundary';
 
 const CodeSnippet = (props) => {
 	const { copy, code, language, showLineNumbers, theme, className } = props;
@@ -33,22 +35,33 @@ const CodeSnippet = (props) => {
 	};
 
 	return (
-		<div className={styles.root}>
-			<SyntaxHighlighter {...syntaxHighlighterProps}>{code}</SyntaxHighlighter>
-			{copy && (
-				<div className={styles.copy} onClick={onCopy}>
-					<CopyIcon
-						className={classes(
-							styles.icon,
-							theme === 'dark' ? styles.dark : styles.light
-						)}
+		<ErrorBoundary
+			FallbackComponent={(args) => {
+				return (
+					<ErrorBoundaryWrapper
+						{...args}
+						className={styles['error-boundary']}
+						custom={custom}
 					/>
-					<div className={copiedState ? styles.copied : styles.notCopied}>
-						{copiedState ? 'Copied' : ''}
+				);
+			}}>
+			<div className={styles.root}>
+				<SyntaxHighlighter {...syntaxHighlighterProps}>{code}</SyntaxHighlighter>
+				{copy && (
+					<div className={styles.copy} onClick={onCopy}>
+						<CopyIcon
+							className={classes(
+								styles.icon,
+								theme === 'dark' ? styles.dark : styles.light
+							)}
+						/>
+						<div className={copiedState ? styles.copied : styles.notCopied}>
+							{copiedState ? 'Copied' : ''}
+						</div>
 					</div>
-				</div>
-			)}
-		</div>
+				)}
+			</div>
+		</ErrorBoundary>
 	);
 };
 
