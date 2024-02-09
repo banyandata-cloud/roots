d73d6b2d141a948e3062ae8f4a555aef36a8be8d
pradeep <pradeep@banyancloud.io>
2024-02-09 10:25:59 +0530

    :bug: toggle selection fix

--- components/Toggle/Toggle.jsx
+++ components/Toggle/Toggle.jsx
@@ -1,17 +1,16 @@
-/* eslint-disable react/no-array-index-key */
 /* eslint-disable no-lonely-if */
 import PropTypes from 'prop-types';
 import { useEffect, useRef, useState } from 'react';
+import { classes } from '../../utils';
 import { Button } from '../buttons';
 import { Skeleton } from './Skeleton';
 import styles from './Toggle.module.css';
 
-// eslint-disable-next-line prefer-arrow-callback
 const Toggle = (props) => {
 	const {
 		options,
 		multi,
-		defaultSelected,
+		defaultValue,
 		theme,
 		value,
 		onChange,
@@ -20,8 +19,6 @@ const Toggle = (props) => {
 		className,
 		smooth,
 	} = props;
-	const [selectedTabs, setSelectedTabs] = useState([]);
-	const [activeTab, setActiveTab] = useState(0);
 	const [sliderLeft, setSliderLeft] = useState(0);
 	const [sliderWidth, setSliderWidth] = useState(0);
 
@@ -35,185 +32,158 @@ const Toggle = (props) => {
 		}
 	};
 
-	// eslint-disable-next-line no-shadow
-	const handleDefaultSelected = (defaultSelected) => {
-		if (defaultSelected === null) {
-			setSelectedTabs([]);
-			setActiveTab(1);
-		} else if (multi && Array.isArray(defaultSelected)) {
-			const defaultSelectedIndexes = options
-				.map((tab, index) => {
-					return defaultSelected.includes(tab.value) ? index + 1 : null;
-				})
-				.filter((index) => {
-					return index !== null;
-				});
-
-			setSelectedTabs(defaultSelectedIndexes);
-			setActiveTab(defaultSelectedIndexes.length > 0 ? defaultSelectedIndexes[0] : 1);
-		} else {
-			const defaultSelectedIndex = options.findIndex((tab) => {
-				return tab.value === defaultSelected;
-			});
-			setSelectedTabs(defaultSelectedIndex !== -1 ? [defaultSelectedIndex + 1] : []);
-			setActiveTab(defaultSelectedIndex !== -1 ? defaultSelectedIndex + 1 : 1);
+	// for uncontrolled input
+	const [uncontrolledValue, setUncontrolledValue] = useState(() => {
+		if (multi) {
+			return defaultValue ?? options?.[0]?.value ?? [];
 		}
-	};
+		return defaultValue ?? options?.[0]?.value;
+	});
 
-	const handleExternalValue = (externalValue) => {
-		const selectedTabIndex = options.findIndex((tab) => {
-			// eslint-disable-next-line no-unused-expressions
-			return tab.value === externalValue;
+	const { current: isControlled } = useRef(value !== undefined);
+
+	// eslint-disable-next-line no-nested-ternary
+	const inputValue = isControlled ? value ?? '' : uncontrolledValue;
+
+	const allSelected = multi ? options.length === inputValue.length : false;
+
+	const onSelectAll = () => {
+		const allValues = options.map((option) => {
+			return option.value;
 		});
-		if (selectedTabIndex !== -1) {
-			setSelectedTabs([selectedTabIndex + 1]);
-			setActiveTab(selectedTabIndex + 1);
+		if (isControlled) {
+			onChange(allValues);
+		} else {
+			setUncontrolledValue(allValues);
 		}
 	};
 
-	useEffect(() => {
-		updateSliderPosition();
-	}, [activeTab, options, selectedTabs]);
-
-	useEffect(() => {
-		handleDefaultSelected(defaultSelected);
-	}, [defaultSelected]);
-
-	useEffect(() => {
-		handleExternalValue(value);
-	}, [value]);
-
-	const handleTabClick = (index) => {
-		if (index === 0) {
-			if (selectedTabs.length === options.length) {
-				setSelectedTabs([]);
-				if (onChange) {
-					onChange(null);
-				}
-			} else {
-				// Select all tabs
-				const allValues = options.map((tab) => {
-					return tab.value;
+	const onButtonClick = (newValue, selected) => {
+		// if multi select
+		if (multi) {
+			// if it was already selected, remove it
+			if (selected) {
+				const newInputValue = inputValue.filter((val) => {
+					return val !== newValue;
 				});
-				setSelectedTabs(
-					Array.from(
-						{
-							length: options.length,
-						},
-						(_, i) => {
-							return i + 1;
-						}
-					)
-				);
-				if (onChange) {
-					onChange(allValues);
-				}
-			}
-			setActiveTab(index);
-		} else {
-			const tab = options[index - 1];
-			if (!tab.disabled) {
-				if (multi) {
-					const updatedSelectedTabs = [...selectedTabs];
-					const tabIndex = updatedSelectedTabs.indexOf(index);
-					if (tabIndex !== -1) {
-						updatedSelectedTabs.splice(tabIndex, 1);
+
+				// if total selected after removal is greater than 0, set that
+				// else select all
+				if (newInputValue.length > 0) {
+					if (isControlled) {
+						onChange(newInputValue);
 					} else {
-						updatedSelectedTabs.push(index);
-					}
-					setSelectedTabs(updatedSelectedTabs);
-					const selectedValues = updatedSelectedTabs.map((selectedIndex) => {
-						return options[selectedIndex - 1].value;
-					});
-					if (onChange) {
-						onChange(selectedValues);
+						setUncontrolledValue(newInputValue);
 					}
 				} else {
-					setSelectedTabs([index]);
-					if (onChange) {
-						onChange(tab.value);
+					onSelectAll();
+				}
+			} else {
+				// if all are selected, select only the one being clicked
+				if (allSelected) {
+					const newInputValue = [newValue];
+					if (isControlled) {
+						onChange(newInputValue);
+					} else {
+						setUncontrolledValue(newInputValue);
+					}
+				} else {
+					const newInputValue = [...inputValue, newValue];
+					if (isControlled) {
+						onChange(newInputValue);
+					} else {
+						setUncontrolledValue(newInputValue);
 					}
 				}
-				setActiveTab(index);
+			}
+		} else {
+			// if single select, just select that item
+			if (isControlled) {
+				onChange(newValue);
+			} else {
+				setUncontrolledValue(newValue);
 			}
 		}
+	};
+
+	const compareSelection = (input, item) => {
+		if (Array.isArray(input)) {
+			return input?.includes(item);
+		}
+		return input === item;
 	};
 	if (loading || fallback) {
 		return <Skeleton theme={theme} fallback={!loading && fallback} />;
 	}
 
+	useEffect(() => {
+		updateSliderPosition();
+	}, [inputValue, options]);
+
 	return (
-		<div
-			className={`${styles['tabs-container']} ${
-				theme === 'dark' ? styles['dark-theme'] : ''
-			} ${className}`}>
-			<div className={styles.tabs}>
+		<div className={classes(styles.root, styles[theme], className)}>
+			<div className={styles.toggle}>
 				{multi && (
 					<Button
 						type='button'
 						size='auto'
 						data-elem='toggle'
 						variant='text'
-						className={` ${
-							selectedTabs.length === options.length
-								? `${styles.selected} ${
-										theme === 'dark' ? styles['dark-theme'] : ''
-								  }`
-								: ''
-						}`}
-						onClick={() => {
-							return handleTabClick(0);
-						}}
-						title='All'>
-						{value}
-					</Button>
+						className={classes(
+							styles['toggle-button'],
+							allSelected ? styles['all-select'] : ''
+						)}
+						onClick={onSelectAll}
+						title='All'
+					/>
 				)}
-				{options.map((tab, index) => {
-					const { title, leftComponent, rightComponent, disabled } = tab;
-					const isActive = index + 1 === activeTab;
+				{options.map((tab) => {
+					const {
+						title,
+						leftComponent,
+						rightComponent,
+						disabled,
+						className: itemClassName,
+					} = tab;
+					const isActive = compareSelection(inputValue, tab.value) && !allSelected;
+
 					return (
-						<div
-							key={index}
-							ref={index + 1 === activeTab ? sliderRef : null}
-							className={`${styles.tab} ${
-								theme === 'dark' ? styles['dark-theme'] : ''
-							} ${index + 1 === activeTab ? styles.active : ''} ${
-								disabled ? styles.disabled : ''
-							} ${
-								multi && selectedTabs.includes(index + 1)
-									? `${styles.selected} ${
-											theme === 'dark' ? styles['dark-theme'] : ''
-									  }`
-									: ''
-							}`}
+						<Button
+							key={title}
+							ref={isActive ? sliderRef : null}
+							type='button'
 							onClick={() => {
-								return handleTabClick(index + 1);
-							}}>
-							<Button
-								type='button'
-								size='auto'
-								variant='text'
-								data-elem='toggle'
-								title={title}
-								className={` ${styles.button} ${
-									theme === 'dark' ? styles['dark-theme'] : ''
-								} ${isActive ? styles.active : ''}`}
-								leftComponent={leftComponent}
-								rightComponent={rightComponent}>
-								{value}
-							</Button>
-						</div>
+								return onButtonClick(tab.value, isActive);
+							}}
+							size='auto'
+							data-elem='toggle'
+							title={title}
+							className={classes(
+								styles['toggle-button'],
+								styles[theme],
+								disabled ? styles.disabled : '',
+								isActive ? styles.active : '',
+								!smooth ? styles.highlight : '',
+								itemClassName
+							)}
+							leftComponent={leftComponent}
+							rightComponent={rightComponent}
+						/>
 					);
 				})}
-				<div
-					className={`${styles['tab-slider']} ${
-						theme === 'dark' ? styles['dark-theme'] : ''
-					} ${smooth ? styles.smooth : ''}`}
-					style={{
-						left: `${sliderLeft}px`,
-						width: `${sliderWidth}px`,
-					}}
-				/>
+				{smooth && !multi && (
+					<div
+						className={classes(
+							styles.slider,
+							styles[theme],
+							smooth ? styles.smooth : ''
+						)}
+						style={{
+							left: `${sliderLeft}px`,
+							width: `${sliderWidth}px`,
+						}}
+					/>
+				)}
 			</div>
 		</div>
 	);
@@ -225,7 +195,6 @@ Toggle.propTypes = {
 	className: PropTypes.string,
 	theme: PropTypes.oneOf(['dark', 'light']),
 	options: PropTypes.arrayOf(PropTypes.string),
-	defaultSelected: PropTypes.string,
 	value: PropTypes.string,
 	multi: PropTypes.bool,
 	smooth: PropTypes.bool,
@@ -237,7 +206,6 @@ Toggle.defaultProps = {
 	className: '',
 	theme: 'light',
 	options: [],
-	defaultSelected: null,
 	value: undefined,
 	multi: false,
 	smooth: false,
