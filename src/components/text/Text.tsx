5ff8e584500dbeb49898dd402038502cb5592438
Pradeep <pradeep@banyancloud.io>
2025-07-20 18:14:29 +0530

    Simplifies Text component props
    Refactors the Text component to simplify its props.
    
    - Removes the generic type from `TextProps` interface, which improves type inference and reduces complexity.
    - Modifies `attrs` prop type to `unknown` for better flexibility.
    - Update TextDOM `attrs` to Record<string, unknown>
    - Adds "light" to TextStroke options

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -7,54 +7,47 @@ import {
 } from 'react';
 import { classes } from '../../utils';
 import styles from './Text.module.css';
-import type { TextComponentType, TextProps } from './types';
+import type { TextProps } from './types';
 
-const Text = forwardRef<RefObject<HTMLElement>, TextProps<TextComponentType>>(
-	(props, ref): ReactElement | null => {
-		const {
-			variant = 'b2',
-			component = 'span',
-			stroke = 'regular',
-			weight,
-			italic,
-			underline,
-			children,
-			className = '',
-			attrs = {},
-		} = props;
+const Text = forwardRef<RefObject<HTMLElement>, TextProps>((props, ref): ReactElement | null => {
+	const {
+		variant = 'b2',
+		component = 'span',
+		stroke = 'regular',
+		weight,
+		italic,
+		underline,
+		children,
+		className = '',
+		attrs = {},
+	} = props;
 
-		const TextDOM = createElement(
-			component,
-			{
-				ref,
-				style: {
-					...(italic && {
-						fontStyle: 'italic',
-					}),
-					...(underline && {
-						textDecoration: 'underline',
-					}),
-					...(weight && {
-						fontWeight: weight,
-					}),
-				},
-				className: classes(
-					styles.root,
-					styles[variant],
-					styles[`${stroke}-stroke`],
-					className
-				),
-				...attrs,
+	const TextDOM = createElement(
+		component,
+		{
+			ref,
+			style: {
+				...(italic && {
+					fontStyle: 'italic',
+				}),
+				...(underline && {
+					textDecoration: 'underline',
+				}),
+				...(weight && {
+					fontWeight: weight,
+				}),
 			},
-			children
-		);
+			className: classes(styles.root, styles[variant], styles[`${stroke}-stroke`], className),
+			...(attrs as Record<string, unknown>),
+		},
+		children
+	);
 
-		if (isValidElement(TextDOM)) {
-			return TextDOM;
-		}
-
-		return null;
+	if (isValidElement(TextDOM)) {
+		return TextDOM;
 	}
-);
+
+	return null;
+});
 
 export default Text;
