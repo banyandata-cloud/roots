da1a037d9f9477fe8f846a72bd3afb6c1226526c
Neeraj B <b.neeraj@banyancloud.io>
2026-01-20 18:27:22 +0530

    BCAPP-6391 : Make the href property to be mandatory

--- src/components/v2/link/types/index.ts
+++ src/components/v2/link/types/index.ts
@@ -2,7 +2,7 @@ type LinkSize = 'sm' | 'md' | 'lg';
 
 export interface LinkProps {
 	children: React.ReactNode;
-	href?: string;
+	href: string;
 	target?: '_blank' | '_self' | '_parent' | '_top';
 	onClick?: () => void;
 
