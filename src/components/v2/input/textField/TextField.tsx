8b6e4e09c421c58cc5314dc6f1fa96e76f5fd70d
NeerajBanyan <b.neeraj@banyancloud.io>
2026-02-02 13:47:52 +0530

    BCAPP-6622 : Added type prop (#878)
    * BCAPP-6622 : Added type prop
    
    * BCAPP-6622 : Link change
    
    * BCAPP-6622 : Fixed snapshot missmatch

--- src/components/v2/input/textField/TextField.tsx
+++ src/components/v2/input/textField/TextField.tsx
@@ -173,6 +173,7 @@ const TextField = forwardRef<HTMLInputElement | HTMLTextAreaElement, TextFieldPr
 			disabled,
 			readOnly,
 			placeholder,
+			type,
 			onFocus: () => {
 				onFocus?.();
 				setShowAutocompleteOptions(true);
@@ -238,7 +239,7 @@ const TextField = forwardRef<HTMLInputElement | HTMLTextAreaElement, TextFieldPr
 							</span>
 						)}
 
-						{type === 'password' && !disabled && linkAction && (
+						{!disabled && linkAction && (
 							<a href='#' onClick={handleLinkClick} className={styles.forgotPassword}>
 								{linkText}
 							</a>
