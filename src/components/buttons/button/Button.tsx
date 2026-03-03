897ef5403fcd1776a2df9a9cc41a4ae6c2516364
ashmitha-t27 <t.ashmitha@banyancloud.io>
2026-03-03 18:31:49 +0530

    Changes made to use custom variants

--- src/components/buttons/button/Button.tsx
+++ src/components/buttons/button/Button.tsx
@@ -4,74 +4,75 @@ import { Button as Buttonv2 } from '../../v2/buttons';
 import { BaseButton } from '../baseButton';
 import styles from './Button.module.css';
 import type { ButtonProps } from './types';
-
+ 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
-	const {
-		className = '',
-		type = 'submit',
-		leftComponent: LeftComponent,
-		title,
-		rightComponent: RightComponent,
-		size = 'sm',
-		flexible,
-		radius = 'default',
-		disabled,
-		onClick,
-		blurOnClick = true,
-		variant = 'contained',
-		color = 'primary',
-		id,
-		textSize = 'sm',
-		v2 = false,
-		dataTestId,
-	} = props;
-
-	if (v2) {
-		return (
-			<Buttonv2
-				ref={ref}
-				className={className}
-				title={title}
-				rightComponent={RightComponent as any}
-				variant={'primary'}
-				size={size as any}
-				textSize={textSize}
-				disabled={disabled}
-				onClick={onClick}
-				blurOnClick={blurOnClick}
-				id={id}
-				type={type}
-			/>
-		);
-	}
-
-	return (
-		<BaseButton
-			{...{
-				ref,
-				type,
-				component1: LeftComponent && <LeftComponent />,
-				title,
-				component3: RightComponent && <RightComponent />,
-				size,
-				flexible,
-				radius,
-				id,
-				disabled,
-				onClick,
-				blurOnClick,
-				variant,
-				dataTestId,
-			}}
-			className={classes(
-				styles.root,
-				styles[`radius-${radius}`],
-				styles[variant],
-				styles[color],
-				className
-			)}
-		/>
-	);
+    const {
+        className = '',
+        type = 'submit',
+        leftComponent: LeftComponent,
+        title,
+        rightComponent: RightComponent,
+        size = 'sm',
+        flexible,
+        radius = 'default',
+        disabled,
+        onClick,
+        blurOnClick = true,
+        variant = 'contained',
+        color = 'primary',
+        id,
+        textSize = 'sm',
+        v2 = false,
+		v2ButtonVariant,
+        dataTestId,
+    } = props;
+ 
+    if (v2) {
+        return (
+            <Buttonv2
+                ref={ref}
+                className={className}
+                title={title}
+                rightComponent={RightComponent}
+                variant={v2ButtonVariant}
+                size={size}
+                textSize={textSize}
+                disabled={disabled}
+                onClick={onClick}
+                blurOnClick={blurOnClick}
+                id={id}
+                type={type}
+            />
+        );
+    }
+ 
+    return (
+        <BaseButton
+            {...{
+                ref,
+                type,
+                component1: LeftComponent && <LeftComponent />,
+                title,
+                component3: RightComponent && <RightComponent />,
+                size,
+                flexible,
+                radius,
+                id,
+                disabled,
+                onClick,
+                blurOnClick,
+                variant,
+                dataTestId,
+            }}
+            className={classes(
+                styles.root,
+                styles[`radius-${radius}`],
+                styles[variant],
+                styles[color],
+                className
+            )}
+        />
+    );
 });
-
+ 
 export default Button;
